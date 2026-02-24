ac20179c38f0c9636e5817408344a0d7c456b3ef
alvaro-frank <alvarojf96@hotmail.com>
2026-02-24 16:13:11 +0000

    refactor: fix domain entities import

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ from app.config import load_configurations, configure_logging
 from app.controllers.webhook_controller import webhook_blueprint
 from app.controllers.requests_controller import requests_blueprint
 from app.infrastructure.database import db
+from app.domain.entities import Request
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -16,7 +17,6 @@ def create_app():
     app.register_blueprint(requests_blueprint)
     
     with app.app_context():
-        from app.domain.models import ServiceRequest
         db.create_all()
 
     return app
